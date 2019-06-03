9bcfb1e1b91842aa9d6080f15434df1e2e7b3cc4
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2019-06-04 01:02:33 +0200

    Added Timeline demo content 

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,41 +1,74 @@
 <template>
-  <v-container>
-    <v-layout text-xs-center wrap>
-      <v-flex>
-        <ol>
-          <li
-            v-for="event in json.events"
-            v-bind:key="event.datetime"
-            class="mb-3"
-          >
-            <time v-bind:datetime="event.datetime"
-              >{{ event.datetime }} Uhr</time
-            >
-            <br />
-            {{ event.title }}
-          </li>
-        </ol>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-timeline>
+    <v-timeline-item
+      v-for="(year, i) in years"
+      :key="i"
+      :color="year.color"
+      small
+    >
+      <template v-slot:opposite>
+        <span
+          :class="`headline font-weight-bold ${year.color}--text`"
+          v-text="year.year"
+        ></span>
+      </template>
+      <div class="py-3">
+        <h2 :class="`headline font-weight-light mb-3 ${year.color}--text`">
+          Lorem ipsum
+        </h2>
+        <div>
+          Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola
+          imperdiet nec ut, sed euismod convenire principes at. Est et nobis
+          iisque percipit, an vim zril disputando voluptatibus, vix an salutandi
+          sententiae.
+        </div>
+      </div>
+    </v-timeline-item>
+  </v-timeline>
 </template>
 
 <script>
 export default {
   data: () => ({
-    json: []
-  }),
-  created: function() {
-    fetch("/media.json")
-      .then(r => r.json())
-      .then(json => {
-        this.json = json;
-      });
-  }
+    years: [
+      {
+        color: "cyan",
+        year: "1960"
+      },
+      {
+        color: "green",
+        year: "1970"
+      },
+      {
+        color: "pink",
+        year: "1980"
+      },
+      {
+        color: "amber",
+        year: "1990"
+      },
+      {
+        color: "orange",
+        year: "2000"
+      }
+    ]
+  })
 };
 </script>
 
-<style scoped lang="stylus">
-ol
-  list-style none
-</style>
+<!--<script>-->
+<!--export default {-->
+<!--  data: () => ({-->
+<!--    json: []-->
+<!--  }),-->
+<!--  created: function() {-->
+<!--    fetch("/media.json")-->
+<!--      .then(r => r.json())-->
+<!--      .then(json => {-->
+<!--        this.json = json;-->
+<!--      });-->
+<!--  }-->
+<!--};-->
+<!--</script>-->
+
+<style scoped lang="stylus"></style>
