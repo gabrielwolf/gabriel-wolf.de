964559a83a5716697c39798f3126984b5d3b7242
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2019-06-03 23:54:17 +0200

    Added dynamic content

---
+++ src/components/Timeline.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex>
+        <ol>
+          <li
+            v-for="event in json.events"
+            v-bind:key="event.datetime"
+            class="mb-3"
+          >
+            <time v-bind:datetime="event.datetime"
+              >{{ event.datetime }} Uhr</time
+            >
+            <br />
+            {{ event.title }}
+          </li>
+        </ol>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    json: []
+  }),
+  created: function() {
+    fetch("/media.json")
+      .then(r => r.json())
+      .then(json => {
+        this.json = json;
+      });
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+ol
+  list-style none
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,24 @@
   <v-container fluid home pt-0>
     <v-layout>
       <v-flex>
-        <v-flex xs12 mt-2 mb-4>
+        <v-flex xs12 mt-2>
           <v-avatar size="200px">
             <img src="/portrait.png" alt="Portrait Gabriel Wolf" />
           </v-avatar>
+          <h1 class="ma-3">
+            Ein Mensch.<br />
+            Ein Archiv.<br />
+            Ein Versuch.
+          </h1>
+          <p class="subheading font-weight-regular">
+            Warum? Vieles aus gutem Grund.<br />
+            Einiges ohne Absicht.<br />
+            Manches zufällig.<br />
+            Nichts umsonst.
+          </p>
+          <h2 class="mb-3 text-xs-center">Ein Zeitstrahl.</h2>
         </v-flex>
-        <HelloWorld />
+        <Timeline />
       </v-flex>
     </v-layout>
   </v-container>
@@ -15,12 +27,12 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Timeline from "@/components/Timeline.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Timeline
   }
 };
 </script>
