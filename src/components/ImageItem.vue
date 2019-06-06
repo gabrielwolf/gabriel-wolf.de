7dc1c222c7d1462b47b2e1fa597464a4202d3d49
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2019-06-06 14:04:52 +0200

    Preload image src computed

---
+++ src/components/ImageItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="root"><img :src="reloadSrc" /></div>
+</template>
+
+<script>
+export default {
+  name: "ImageItem",
+  props: {
+    title: String,
+    src: String
+  },
+  computed: {
+    reloadSrc: function() {
+      return this.src.split(".jpg")[0] + "-preview.jpg";
+    }
+  }
+};
+</script>
+
+<style scoped></style>
