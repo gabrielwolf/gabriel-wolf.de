7409652571a5816d453bc0ee552e5160164ad03d
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2019-06-13 10:23:43 +0200

    Content Copyleft

--- src/views/Copyleft.vue
+++ src/views/Copyleft.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid copyleft pt-0>
     <v-layout>
-      <v-flex>
+      <v-flex xs12 sm6 offset-sm3 xl4 offset-xl4>
         <h1 class="mb-3">Copyleft</h1>
         <a
           rel="license"
@@ -11,24 +11,15 @@
             src="cc-by-icon.svg"
             class="mb-3"/></a
         ><br />
+
         <p text-xs-justify>
-          Alle Werke dieser Website, deren Urheber Gabriel Wolf ist, sind unter
-          einer
+          Alle Werke dieser Website sind unter einer
           <a
             rel="license"
             href="https://creativecommons.org/licenses/by/4.0/deed.de"
             >Creative Commons Namensnennung 4.0 International Lizenz</a
           >
-          lizensiert.
-        </p>
-        <p
-          :class="{
-            'mb-2': $vuetify.breakpoint.smAndDown,
-            'mb-0': $vuetify.breakpoint.mdAndUp
-          }"
-        >
-          Das heißt, sie dürfen frei verwendet werden, solange mein Name mit
-          angegeben wird. Ausnahmen versuche ich zu kennzeichnen.
+          lizensiert. Davon abweichend sind andere Urheber gekennzeichnet.
         </p>
       </v-flex>
     </v-layout>
